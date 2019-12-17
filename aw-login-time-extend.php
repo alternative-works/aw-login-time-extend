5df2caf3d243bdadfde3c357e6e02546f7b1f2a1
alternative-works <takanashi.blog@gmail.com>
2019-12-17 23:05:01 +0900

    Github更新通知が来ない不具合を再修正

--- aw-login-time-extend.php
+++ aw-login-time-extend.php
@@ -4,12 +4,25 @@ Plugin Name: AW Login Time Extend
 Plugin URI: https://careru.jp/
 Description: ログイン期限を実質無期限にする
 Author: Alternative Works
-Version: 0.0.3
+Version: 0.0.2
 Author URI: https://careru.jp/
 */
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-require_once( dirname( __FILE__ ) . '/update-checker/update-checker.php');
+if(!defined('UPDATE_CHECKER_LOADED')){
+    require_once( dirname( __FILE__ ) . '/update-checker/vendor/autoload.php' );
+}
+define('UPDATE_CHECKER_LOADED',true);
+
+add_action( 'init', function(){
+        $plugin_slug = plugin_basename( __FILE__ ); // e.g. `hello/hello.php`.
+        $gh_user = 'alternative-works';                      // The user name of GitHub.
+        $gh_repo = 'aw-login-time-extend';       // The repository name of your plugin.
+
+        // Activate automatic update.
+        new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
+    }
+);
 require_once( dirname( __FILE__ ) . '/common/main.php' );
