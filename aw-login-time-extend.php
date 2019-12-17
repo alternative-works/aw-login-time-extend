74ed84c883b09349b76dcadb70feda0b6a667c58
alternative-works <takanashi.blog@gmail.com>
2019-12-17 23:13:02 +0900

    プラグインバージョン番号の修正

--- aw-login-time-extend.php
+++ aw-login-time-extend.php
@@ -4,7 +4,7 @@ Plugin Name: AW Login Time Extend
 Plugin URI: https://careru.jp/
 Description: ログイン期限を実質無期限にする
 Author: Alternative Works
-Version: 0.0.2
+Version: 0.0.5
 Author URI: https://careru.jp/
 */
 if ( ! defined( 'ABSPATH' ) ) {
